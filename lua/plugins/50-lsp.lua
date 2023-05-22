1d2561beaeee130a6faa5e408ac9710491dbd148
leafee98 <leafee98@hotmail.com>
2023-05-22 15:17:26 +0800

    add new lsp: pylsp

--- lua/plugins/50-lsp.lua
+++ lua/plugins/50-lsp.lua
@@ -51,7 +51,7 @@ return {
         config = function (_, opts)
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local servers = { "clangd", "rust_analyzer", "pyright", "gopls", "denols" }
+            local servers = { "clangd", "rust_analyzer", "pyright", "gopls", "denols", "pylsp" }
             for _, lsp in ipairs(servers) do
                 require("lspconfig")[lsp].setup{
                     capabilities = capabilities,
